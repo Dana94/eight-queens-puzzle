43af8fe40853e66aa8b6b1d3b21fbbdff1c8cc3a
Dana <dana.ottaviani@gmail.com>
2020-12-11 18:10:00 -0500

    fix: add square border

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="square" :class="{dark: dark, light: !dark}" @click="showQueen = !showQueen">
+  <div class="square"
+    :class="{dark: dark, light: !dark, 'left-border': leftBorder, 'right-border': rightBorder, 'top-border': topBorder, 'bottom-border': bottomBorder}"
+    @click="showQueen = !showQueen"
+  >
     <img :src="queen" alt="Queen" class="queen" v-if="showQueen" />
   </div>
 </template>
@@ -27,6 +30,18 @@ export default {
         // only coordiantes that are not both even or both odd values are dark
         return (this.index_x % 2 === 0 && this.index_y % 2 !== 0) || (this.index_x % 2 !== 0 && this.index_y % 2 === 0)
       },
+      leftBorder() {
+        return this.index_y === 0;
+      },
+      rightBorder() {
+        return this.index_y === 7;
+      },
+      topBorder() {
+        return this.index_x === 0;
+      },
+      bottomBorder() {
+        return this.index_x === 7;
+      },
       queen() {
         return this.dark ? require("../assets/white-queen.svg") : require("../assets/black-queen.svg");
       }
@@ -41,6 +56,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  border: 2px solid #cacaca;
 }
 .square:hover {
   cursor: pointer;
@@ -55,5 +71,18 @@ export default {
 .queen {
   height: 70%;
   width: 70%;
+}
+
+.square.left-border {
+  border-left-width: 4px;
+}
+.square.right-border {
+  border-right-width: 4px;
+}
+.square.top-border {
+  border-top-width: 4px;
+}
+.square.bottom-border {
+  border-bottom-width: 4px;
 }
 </style>
