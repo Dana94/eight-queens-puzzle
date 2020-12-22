41d18fc055d3a2334b3f4f2043c0693fbed1c93c
Dana <dana.ottaviani@gmail.com>
2020-12-22 09:56:22 -0500

    feat: change theme button

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
   <div id="app">
     <header>
+      <button aria-label="Change board theme" @click="setTheme" class="theme" :class="{'light': lightTheme, 'dark': !lightTheme}"></button>
       <h1>8 Queens Puzzle</h1>
-      <button @click="setTheme">
-        <span v-if="!lightTheme">Light</span>
-        <span v-else>Dark</span>
-        Theme
-      </button>
+      <button @click="clear" class="clear">Clear Board</button>
     </header>
     <Board />
   </div>
@@ -29,6 +26,9 @@ export default {
     setTheme() {
       this.lightTheme = !this.lightTheme;
       this.$store.dispatch('setTheme', this.lightTheme ? 'light' : 'dark');
+    },
+    clear() {
+      this.$store.dispatch('clearBoard');
     }
   }
 }
@@ -48,12 +48,30 @@ export default {
   flex-direction: column;
 }
 
-h1 {
-  font-family: "Playfair Display", serif;
+header {
   background-color: white;
   border-radius: 10px;
   max-width: 796px; /*work on this*/
   margin: 0 auto;
   padding: 2rem;
+  display: flex;
+  flex-direction: column;
+}
+h1 {
+  font-family: "Playfair Display", serif;
+}
+
+button.theme {
+  border: none;
+  padding: 1rem;
+  width: 4rem;
+  align-self: flex-end;
+}
+button.theme.light {
+  background: linear-gradient(106deg, rgba(0,0,0,1) 0%, rgba(255,255,255,1) 100%);
+}
+
+button.theme.dark {
+  background: linear-gradient(106deg,rgb(209, 139, 71) 0%, rgb(255, 206, 158) 100%);
 }
 </style>
