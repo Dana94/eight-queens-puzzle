6815d16a0e1014725b1e876d31c170842140c917
Dana <dana.ottaviani@gmail.com>
2021-01-09 09:35:55 -0500

    feat: finish edits

--- src/store/board.js
+++ src/store/board.js
@@ -94,12 +94,10 @@ const getters = {
     invalidMove: (state) => (coords) => {
         let invalid = false;
 
-        //optimize
         for (let [key, set] of state.board.entries()) {
-
             // check rows and columns
             let keyArr = key.split(",");
-            //stop checking board coords when it reaches its own coords (otherwise it will find itself and say it's invalid)
+            //don't check its own coords (otherwise it will find itself and say it's invalid)
             if (!(parseInt(keyArr[0]) === coords.x && parseInt(keyArr[1]) === coords.y)) {
 
                 //rows and columns (only matches the x OR the y, not both)
@@ -107,7 +105,6 @@ const getters = {
                     invalid = true;
                 }
                 //check diagonals
-                // wouldn't .has() work here?
                 for (let val of set.entries()) {
                     if (val[0][0] === coords.x && val[0][1] === coords.y) {
                         invalid = true;
